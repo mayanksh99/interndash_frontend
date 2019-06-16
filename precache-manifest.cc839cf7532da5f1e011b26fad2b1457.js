d79911062954c413e4156b6d63748f181faed6a6
Aakash Goel <goelaakash79@gmail.com>
2019-06-17 01:12:29 +0530

    Updates

---
+++ precache-manifest.cc839cf7532da5f1e011b26fad2b1457.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a19d155cd69268f7ff5fb24e5a9220dd",
+    "url": "/interndash_frontend/index.html"
+  },
+  {
+    "revision": "b929de277c6d6a3c2064",
+    "url": "/interndash_frontend/static/css/2.3e9bd1b9.chunk.css"
+  },
+  {
+    "revision": "900c77cc2b5a2c1b54bb",
+    "url": "/interndash_frontend/static/css/main.580e8e33.chunk.css"
+  },
+  {
+    "revision": "b929de277c6d6a3c2064",
+    "url": "/interndash_frontend/static/js/2.f6408fb7.chunk.js"
+  },
+  {
+    "revision": "900c77cc2b5a2c1b54bb",
+    "url": "/interndash_frontend/static/js/main.e520c086.chunk.js"
+  },
+  {
+    "revision": "a3ad42d13794a7446455",
+    "url": "/interndash_frontend/static/js/runtime~main.6ff832e6.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/interndash_frontend/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/interndash_frontend/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/interndash_frontend/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/interndash_frontend/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/interndash_frontend/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "92c4baae12b3c4ecbfeb2982c3374b77",
+    "url": "/interndash_frontend/static/media/logo.92c4baae.png"
+  },
+  {
+    "revision": "8b444a5fd46f79151edcbb52b8051902",
+    "url": "/interndash_frontend/static/media/register.8b444a5f.png"
+  }
+]);
